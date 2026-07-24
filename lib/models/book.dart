c3b657ea28160db6fd02e6b3a2db77ff655411bd
ls-matheus <leonato289@gmail.com>
2026-07-24 15:16:36 -0300

    feat: add local text file caching for books

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -6,6 +6,24 @@ class Book {
 
   Book(this.id, this.title, this.author, {this.isBorrowed = false});
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'author': author,
+      'isBorrowed': isBorrowed,
+    };
+  }
+
+  factory Book.fromMap(Map<String, dynamic> map) {
+    return Book(
+      map['id'],
+      map['title'],
+      map['author'],
+      isBorrowed: map['isBorrowed'] ?? false,
+    );
+  }
+
   @override
   String toString() {
     return 'ID: $id | Título: $title | Autor: $author | Emprestado: ${isBorrowed ? "Sim" : "Não"}';
--- lib/models/library.dart
+++ lib/models/library.dart
@@ -1,10 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
 import 'book.dart';
 
 class Library {
   List<Book> books = [];
+  final String _cachePath = 'Cache/books.txt';
+
+  Library() {
+    _loadBooks();
+  }
+
+  void _loadBooks() {
+    var file = File(_cachePath);
+    if (file.existsSync()) {
+      var contents = file.readAsStringSync();
+      if (contents.isNotEmpty) {
+        List<dynamic> jsonList = jsonDecode(contents);
+        books = jsonList.map((item) => Book.fromMap(item)).toList();
+      }
+    }
+  }
+
+  void _saveBooks() {
+    var directory = Directory('Cache');
+    if (!directory.existsSync()) {
+      directory.createSync();
+    }
+    var file = File(_cachePath);
+    var jsonList = books.map((b) => b.toMap()).toList();
+    file.writeAsStringSync(jsonEncode(jsonList));
+  }
 
   void addBook(Book book) {
     books.add(book);
+    _saveBooks();
     print('Livro "${book.title}" cadastrado com sucesso!');
   }
 
@@ -34,6 +63,7 @@ class Library {
     if (index != -1) {
       if (!books[index].isBorrowed) {
         books[index].isBorrowed = true;
+        _saveBooks();
         print('Livro emprestado com sucesso!');
       } else {
         print('O livro já está emprestado.');
@@ -48,6 +78,7 @@ class Library {
     if (index != -1) {
       if (books[index].isBorrowed) {
         books[index].isBorrowed = false;
+        _saveBooks();
         print('Livro devolvido com sucesso!');
       } else {
         print('O livro não estava emprestado.');
@@ -61,6 +92,7 @@ class Library {
     var index = books.indexWhere((b) => b.id == id);
     if (index != -1) {
       books.removeAt(index);
+      _saveBooks();
       print('Livro removido com sucesso!');
     } else {
       print('Livro não encontrado.');
